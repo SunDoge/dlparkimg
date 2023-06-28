bb43d7975834ea837833ac786e2b614ab2bcbbb0
SunDoge <384813529@qq.com>
2023-06-28 17:55:19 +0800

    update dlpark

--- src/lib.rs
+++ src/lib.rs
@@ -1,49 +1,38 @@
 use std::ffi::c_void;
 
-use dlpark::{
-    ffi::{DataType, Device},
-    tensor::{
-        traits::{AsTensor, HasByteOffset, HasData, HasDevice, HasDtype, HasShape, HasStrides},
-        ManagedTensor, ManagerCtx,
-    },
-};
+use dlpark::prelude::*;
 use image::Rgb;
 use pyo3::prelude::*;
 
 struct PyRgbImage(image::RgbImage);
 
-impl HasData for PyRgbImage {
-    fn data(&self) -> *mut std::ffi::c_void {
+impl ToTensor for PyRgbImage {
+    fn data_ptr(&self) -> *mut std::ffi::c_void {
         self.0.as_ptr() as *const c_void as *mut c_void
     }
-}
 
-impl HasDevice for PyRgbImage {
-    fn device(&self) -> dlpark::ffi::Device {
-        Device::CPU
+    fn byte_offset(&self) -> u64 {
+        0
     }
-}
 
-impl HasDtype for PyRgbImage {
-    fn dtype(&self) -> dlpark::ffi::DataType {
-        DataType::U8
-    }
-}
-
-impl HasShape for PyRgbImage {
-    fn shape(&self) -> dlpark::tensor::Shape {
-        dlpark::tensor::Shape::Owned(
+    fn shape(&self) -> CowIntArray {
+        CowIntArray::from_owned(
             [self.0.height(), self.0.width(), 3]
                 .map(|x| x as i64)
                 .to_vec(),
         )
     }
-}
 
-impl HasStrides for PyRgbImage {}
-impl HasByteOffset for PyRgbImage {
-    fn byte_offset(&self) -> u64 {
-        0
+    fn device(&self) -> Device {
+        Device::CPU
+    }
+
+    fn dtype(&self) -> DataType {
+        DataType::U8
+    }
+
+    fn strides(&self) -> Option<CowIntArray> {
+        None
     }
 }
 
@@ -57,19 +46,11 @@ fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
 fn read_image(filename: &str) -> ManagerCtx<PyRgbImage> {
     let img = image::open(filename).unwrap();
     let rgb_img = img.to_rgb8();
-    PyRgbImage(rgb_img).into()
+    ManagerCtx::new(PyRgbImage(rgb_img))
 }
 
 #[pyfunction]
 fn write_image(filename: &str, tensor: ManagedTensor) {
-    // let v: Vec<u8> = unsafe {
-    //     Vec::from_raw_parts(
-    //         tensor.data::<u8>() as *mut u8,
-    //         tensor.num_elements(),
-    //         tensor.num_elements(),
-    //     )
-    // };
-
     let buf = tensor.as_slice::<u8>();
 
     let rgb_img = image::ImageBuffer::<Rgb<u8>, _>::from_raw(
